2f4ed496eef88345413692de4fbc32a63d01aaaa
darlish <michelmbama01@gmail.com>
2024-04-16 19:53:45 +0200

    Initial commit

---
+++ src/Personne.java
@@ -0,0 +1,16 @@
+public class Personne {
+    protected String nom;
+    protected int age;
+    protected char sex;
+
+    private Personne(int a {
+
+    }
+
+    char s; String n){
+        this.nom = nom;
+        this.age = a;
+        this.sex = s;
+
+    }
+}
